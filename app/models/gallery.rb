7d498521bc0ec5e59b7e3ec61b5bee11cf754a08
Gilles Mathurin <gillesmath@me.com>
2009-06-19 21:43:34 -0400

    optimisation de la ressource gallery pour meilleure gestion de leur affichage

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,12 +1,12 @@
 class Gallery < ActiveRecord::Base
   belongs_to :user
   
-  has_attached_file :photo1, :styles => { :thumb => "100x100>", :medium => "300x300>" }
-  has_attached_file :photo2, :styles => { :thumb => "100x100>", :medium => "300x300>" }
-  has_attached_file :photo3, :styles => { :thumb => "100x100>", :medium => "300x300>" }
-  has_attached_file :photo4, :styles => { :thumb => "100x100>", :medium => "300x300>" }
-  has_attached_file :photo5, :styles => { :thumb => "100x100>", :medium => "300x300>" }
-  has_attached_file :photo6, :styles => { :thumb => "100x100>", :medium => "300x300>" }
+  has_attached_file :photo1, :styles => { :thumb => "100x100>", :medium => "300x300>" }, :default_url => ""
+  has_attached_file :photo2, :styles => { :thumb => "100x100>", :medium => "300x300>" }, :default_url => ""
+  has_attached_file :photo3, :styles => { :thumb => "100x100>", :medium => "300x300>" }, :default_url => ""
+  has_attached_file :photo4, :styles => { :thumb => "100x100>", :medium => "300x300>" }, :default_url => ""
+  has_attached_file :photo5, :styles => { :thumb => "100x100>", :medium => "300x300>" }, :default_url => ""
+  has_attached_file :photo6, :styles => { :thumb => "100x100>", :medium => "300x300>" }, :default_url => ""
   
   # fetch the last four 4 galleries
   named_scope :last_four, :order => "created_at desc", :limit => 4
