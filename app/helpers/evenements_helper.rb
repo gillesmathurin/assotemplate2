9b8fdd0f24b157df5afa1e17c509eb29acc83d03
Gilles Mathurin <gillesmath@mac.com>
2009-06-19 23:14:07 -0400

    installation du plugin later_dude et configuration pour son utilisation de la ressource evenement

--- app/helpers/evenements_helper.rb
+++ app/helpers/evenements_helper.rb
@@ -1,2 +1,19 @@
 module EvenementsHelper
+  def calendar_events_proc
+    lambda do |day|
+      if Evenement.has_events_on?(day)
+          [link_to(day.day, evenenemts_du_jour_path(day)), {}]
+      else
+          day.day
+      end
+    end
+  end
+  
+  def has_photo?(evenement)
+    if evenement.photo.url != "/photos/original/missing.png"
+      return true
+    else
+      return false
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :comments
 
   map.resources :evenements
-
+  map.evenenemts_du_jour '/evenements_du_jour/:day', :controller => 'evenements', :action => 'evenenemts_du_jour', :day => nil
+  map.evenements_du_mois '/evenements_du_mois/:day', :controller => 'evenements', :action => 'evenements_du_mois', :day => nil
+  
   map.resources :faqs
 
   map.resources :galleries
