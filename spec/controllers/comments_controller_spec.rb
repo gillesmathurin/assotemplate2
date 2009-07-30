a1fb8a0aad0d07e20a82a1b1e67a156a4faf4e52
Gilles Mathurin <gillesmath@me.com>
2009-07-30 15:25:27 -0400

    modification de comments_controller_spec

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -6,34 +6,55 @@ describe CommentsController do
     @mock_comment ||= mock_model(Comment, stubs)
   end
   
-  describe "GET index" do
+  def mock_article(stubs={})
+    @mock_article ||= mock_model(Article, stubs)
+  end
+  
+  before(:all) do
+    Article.should_receive(:find).with("1").and_return(@mock_article)
+  end
+  
+  describe "GET index" do  
     it "assigns all comments as @comments" do
-      Comment.stub!(:find).with(:all).and_return([mock_comment])
-      get :index
+      @mock_article.should_receive(:comments).and_return([mock_comment])
+      get :index, :article_id => "1"
       assigns[:comments].should == [mock_comment]
     end
   end
 
   describe "GET show" do
+    
+    before(:each) do
+      @prox_comments = mock('comments')
+    end
+        
     it "assigns the requested comment as @comment" do
-      Comment.stub!(:find).with("37").and_return(mock_comment)
-      get :show, :id => "37"
-      assigns[:comment].should equal(mock_comment)
+      @mock_article.should_receive(:comments).and_return(@prox_comments)
+      @prox_comments.should_receive(:find).with("37").and_return(mock_comment)
+      get :show, :article_id => "1", :id => "37"
+      assigns[:comment].should == (mock_comment)
     end
   end
 
   describe "GET new" do
+    
+    before(:each) do
+      
+    end
+    
     it "assigns a new comment as @comment" do
-      Comment.stub!(:new).and_return(mock_comment)
-      get :new
-      assigns[:comment].should equal(mock_comment)
+      @mock_article.should_receive(:comments).and_return(@prox_comments)
+      @prox_comments.should_receive(:build).and_return(mock_comment)
+      get :new, :article_id => "1"
+      assigns[:comment].should == (mock_comment)
     end
   end
 
   describe "GET edit" do
     it "assigns the requested comment as @comment" do
-      Comment.stub!(:find).with("37").and_return(mock_comment)
-      get :edit, :id => "37"
+      @mock_article.should_receive(:comments).and_return(@prox_comments)
+      @prox_comments.should_receive(:find).with("37").and_return(mock_comment)
+      get :show, :article_id => "1", :id => "37"
       assigns[:comment].should equal(mock_comment)
     end
   end
@@ -42,27 +63,31 @@ describe CommentsController do
     
     describe "with valid params" do
       it "assigns a newly created comment as @comment" do
-        Comment.stub!(:new).with({'these' => 'params'}).and_return(mock_comment(:save => true))
-        post :create, :comment => {:these => 'params'}
+        @mock_article.should_receive(:comments).and_return(@prox_comments)
+        @prox_comments.should_receive(:build).with({'these' => 'params'}).and_return(mock_comment(:save => true))
+        post :create, :comment => {:these => 'params'}, :article_id => "1"
         assigns[:comment].should equal(mock_comment)
       end
 
       it "redirects to the created comment" do
-        Comment.stub!(:new).and_return(mock_comment(:save => true))
-        post :create, :comment => {}
+        @mock_article.should_receive(:comments).and_return(@prox_comments)
+        @prox_comments.should_receive(:build).with({}).and_return(mock_comment(:save => true))
+        post :create, :comment => {}, :article_id => "1"
         response.should redirect_to(comment_url(mock_comment))
       end
     end
     
     describe "with invalid params" do
       it "assigns a newly created but unsaved comment as @comment" do
-        Comment.stub!(:new).with({'these' => 'params'}).and_return(mock_comment(:save => false))
+        @mock_article.should_receive(:comments).and_return(@prox_comments)
+        @prox_comments.should_receive(:build).with({}).and_return(mock_comment(:save => false))
         post :create, :comment => {:these => 'params'}
         assigns[:comment].should equal(mock_comment)
       end
 
       it "re-renders the 'new' template" do
-        Comment.stub!(:new).and_return(mock_comment(:save => false))
+        @mock_article.should_receive(:comments).and_return(@prox_comments)
+        @prox_comments.should_receive(:build).with({}).and_return(mock_comment(:save => false))
         post :create, :comment => {}
         response.should render_template('new')
       end
