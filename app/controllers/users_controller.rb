f21eb980a9db96432e180bf5199e6b0257ba37db
Gilles Mathurin <gillesmath@me.com>
2009-08-11 23:55:39 -0400

    modification de la ressource user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
   end
   
   def edit
-      @membre = User.find(params[:id])
+      @user = User.find(params[:id])
   end
  
   def create
@@ -40,6 +40,21 @@ class UsersController < ApplicationController
     else
       flash[:error]  = "Désolé, nous n'avons pas pu créer ce compte. Essayer encore."
       render :action => 'new'
+    end
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        flash[:notice] = "Modifications enregistrées"
+        format.html { redirect_to(@user) }
+        format.xml { head :ok}
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
+      end
     end
   end
 
