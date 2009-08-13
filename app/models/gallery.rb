66120b0826caef0accf5e9b2b079a296659f149f
Gilles Mathurin <gillesmath@me.com>
2009-08-13 00:29:02 -0400

    meilleure gestion des galleries d'images

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -6,7 +6,6 @@ class Gallery < ActiveRecord::Base
   has_attached_file :photo3, :styles => { :thumb => "100x100>", :medium => "300x300>" }, :default_url => ""
   has_attached_file :photo4, :styles => { :thumb => "100x100>", :medium => "300x300>" }, :default_url => ""
   has_attached_file :photo5, :styles => { :thumb => "100x100>", :medium => "300x300>" }, :default_url => ""
-  has_attached_file :photo6, :styles => { :thumb => "100x100>", :medium => "300x300>" }, :default_url => ""
   
   # fetch the last four 4 galleries
   named_scope :last_four, :order => "created_at desc", :limit => 4
