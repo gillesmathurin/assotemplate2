c53b3084571bc93a0f471aa14ad2e2ace4438d3a
Gilles Mathurin <gillesmath@me.com>
2009-08-13 14:31:41 -0400

    reconfiguration de l'initializer mail.rb

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,22 +1,30 @@
+# ActionMailer::Base.delivery_method = :smtp
+# ActionMailer::Base.smtp_settings = { 
+#   :address => "mail.crajep971.fr", 
+#   :port => 26, 
+#   :domain => "crajep971.fr",
+#   :authentication => :login, 
+#   :user_name => "gillesmath+crajep971.fr", 
+#   :password => "30carmen30"
+# }
+# ActionMailer::Base.perform_deliveries = true
+# ActionMailer::Base.raise_delivery_errors = true
+# ActionMailer::Base.default_charset = "iso-8859-1"
+
+Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
 ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = { 
-  :address => "mail.crajep971.fr", 
-  :port => 26, 
-  :domain => "crajep971.fr",
-  :authentication => :login, 
-  :user_name => "gillesmath+crajep971.fr", 
-  :password => "30carmen30"
-}
 ActionMailer::Base.perform_deliveries = true
 ActionMailer::Base.raise_delivery_errors = true
-ActionMailer::Base.default_charset = "iso-8859-1"
+ActionMailer::Base.default_charset = "utf-8"
 
-# ActionMailer::Base.delivery_method = :sendmail
-# ActionMailer::Base.sendmail_settings = {
-#  :location       => '/usr/sbin/sendmail',
-#  :arguments      => '-i -t '
-# }                                                                                                                                                                                                                             
-# ActionMailer::Base.perform_deliveries = true                                                                                            
-# ActionMailer::Base.raise_delivery_errors = true                                                                                           
-# ActionMailer::Base.default_charset = "utf-8"
+ActionMailer::Base.smtp_settings = {
+  :enable_starttls_auto => true, 
+  :address              => "smtp.gmail.com", 
+  :port                 => 587, 
+  :domain               => 'gmwebagency.com',
+  :authentication       => :login,
+  :user_name            => "gwadaman30@gmail.com", 
+  :password             => "carmen30", 
+}
+
 
